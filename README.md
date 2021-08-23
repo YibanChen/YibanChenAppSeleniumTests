985831170ce6195c9cc07b8e93dba7a655b464b7
David Rhodus <david@vacovideo.com>
2021-08-23 15:51:24 -0700

    add Selenium Test for the YibanChen React App

---
+++ README.md
@@ -0,0 +1,21 @@
+# YibanChen Integration Tests
+
+## Setup
+
+Please make sure you are running the YibanChen build locally and that it is reachable at http://localhost:3000
+
+Also make sure that you do not already have Google Chrome opened.
+
+## Running the tests
+
+To start the tests, cd into the testing directory and enter the command
+
+`python3 tests.py`
+
+Some manual configuration is required after running the above command before the automated tests can begin.
+
+Selenium will open a chrome browser and navigate to the YibanChen site. Manually sign into a Polkadot account via the Polkadot extension, or create a new one.
+
+After signing in via the extension, reload the page and then select your account from the dropdown menu. At this point, no more input is required and you can wait for the automated tests to complete.
+
+The integration tests are run as a monolith. This is because each Python unit test would require the user to again sign in via the extension. Running the tests as a monolith avoids this problem with minimal complications.
---
+++ tests.py
@@ -0,0 +1,116 @@
+import unittest, time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+
+
+class YibanChenTestSuite(unittest.TestCase):
+    def setUp(self):
+        ext_dir = "extension_0_38_3_0.crx.crx"
+
+        chrome_options = Options()
+        chrome_options.add_extension(ext_dir)
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    @classmethod
+    def setUpClass(cls):
+
+        ext_dir = "extension_0_38_3_0.crx.crx"
+
+        chrome_options = Options()
+        chrome_options.add_extension(ext_dir)
+        chrome_options.add_argument("user-data-dir=/Users/maxwellrosenberg/Library/Application Support/Google/Chrome/")
+        driver = webdriver.Chrome(options=chrome_options)
+
+    def test_monolith(self):
+        driver = self.driver
+        driver.get("http://localhost:3000/inbox")
+        self.assertIn("YibanChen", driver.title)
+
+        # Use these 45 seconds to sign into the polkadot extension. If more time is required, increase the number of seconds.
+
+        time.sleep(45)
+
+        # Inbox test
+
+        try:
+            reply_button = driver.find_element_by_xpath('//*[@id="wide"]/div/div[1]/div/button[1]')
+            reply_button.click()
+            webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+        except:
+            no_messages = driver.find_element_by_xpath('//*[@id="parent"]/div[1]/div/p').text
+            self.assertEqual("No messages found", no_messages)
+
+        # Composition Test
+
+        compose_button = driver.find_element_by_xpath('//*[@id="responsive-navbar-nav"]/div[1]/a[2]')
+        compose_button.click()
+
+        to_field = driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[1]/textarea')
+        to_field.clear()
+        to_field.send_keys("5H3tev113c7xnoLV8NJMAUFg15Egs4SR1sakKRpPQbszjvFd")
+        body_field = driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[3]/textarea')
+        body_field.clear()
+        body_field.send_keys("body of message")
+        send_button = driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[4]/button')
+        send_button.click()
+
+        # Wait for the alert to be displayed
+        time.sleep(1)
+
+        # Store the alert in a variable for reuse
+        alert = driver.switch_to.alert
+
+        # Store the alert text in a variable
+        alert_text = alert.text
+        self.assertEqual("You are about to send your message with no subject. Continue?", alert_text)
+
+        # Press the Cancel button
+        alert.dismiss()
+
+        body_field.clear()
+
+        to_field = driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[1]/textarea')
+        to_field.clear()
+        # Junk wallet
+        to_field.send_keys("bad address")
+        subject_field = driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[2]/textarea')
+        subject_field.clear()
+        body_field = driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[3]/textarea')
+        send_button = driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div[4]/button')
+        send_button.click()
+        time.sleep(1)
+
+        expected_error_message = driver.find_element_by_xpath('//*[@id="root"]/div/div/p').text
+        self.assertIn("The recipient address you have entered is not a valid Polkadot address.", expected_error_message)
+        back_button = driver.find_element_by_xpath('//*[@id="root"]/div/div/button')
+        back_button.click()
+        time.sleep(1)
+
+        # Settings page test
+
+        settings_button = driver.find_element_by_xpath('//*[@id="responsive-navbar-nav"]/div[1]/a[3]')
+        settings_button.click()
+
+        pinata_key_button = driver.find_element_by_xpath('//*[@id="root"]/div/div/div[3]/form/label/div/div[1]')
+        pinata_key_button.click()
+        time.sleep(5)
+
+        balance_text = driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/h5').text
+
+        self.assertIn("Balance", balance_text)
+
+        account_dropdown_button = driver.find_element_by_xpath('//*[@id="dropdown-basic"]')
+        account_dropdown_button.click()
+
+        account_select_button = driver.find_element_by_xpath('//*[@id="root"]/div/div/div[1]/div/div/div[1]/a')
+        account_select_button.click()
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
